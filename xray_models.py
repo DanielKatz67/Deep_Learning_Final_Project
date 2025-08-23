99ab717cf78a4a99bff60f9d1e4f245b7bfa78c6
Daniel Katz <danielka1010@gmail.com>
2025-08-23 12:58:07 +0300

    delete comments

--- xray_models.py
+++ xray_models.py
@@ -55,118 +55,6 @@ class LayerNorm2d(nn.Module):
         x = x.permute(0, 3, 1, 2).contiguous()  # (N, C, H, W)
         return x
 
-
-# # -----------------------------
-# # Vision Transformer (ViT)
-# # -----------------------------
-# class PatchEmbedding(nn.Module):
-#     def _init_(self, in_channels, patch_size, embed_dim):
-#         super()._init_()
-#         self.patch_size = patch_size
-#         self.proj = nn.Conv2d(in_channels, embed_dim, kernel_size=patch_size, stride=patch_size)
-#
-#     def forward(self, x):
-#         x = self.proj(x)  # (B, E, H', W')
-#         x = x.flatten(2)  # (B, E, H'*W')
-#         x = x.transpose(1, 2)  # (B, N, E) where N is number of patches
-#         return x
-#
-#
-# class Attention(nn.Module):
-#     def _init_(self, embed_dim, num_heads):
-#         super()._init_()
-#         self.num_heads = num_heads
-#         self.head_dim = embed_dim // num_heads
-#         self.scale = self.head_dim ** -0.5
-#         self.qkv = nn.Linear(embed_dim, embed_dim * 3, bias=False)
-#         self.proj = nn.Linear(embed_dim, embed_dim)
-#
-#     def forward(self, x):
-#         B, N, C = x.shape
-#         qkv = self.qkv(x).reshape(B, N, 3, self.num_heads, self.head_dim).permute(2, 0, 3, 1, 4)
-#         q, k, v = qkv[0], qkv[1], qkv[2]
-#
-#         attn = (q @ k.transpose(-2, -1)) * self.scale
-#         attn = attn.softmax(dim=-1)
-#
-#         x = (attn @ v).transpose(1, 2).reshape(B, N, C)
-#         x = self.proj(x)
-#         return x
-#
-#
-# class MLP(nn.Module):
-#     def _init_(self, in_features, hidden_features, out_features):
-#         super()._init_()
-#         self.fc1 = nn.Linear(in_features, hidden_features)
-#         self.act = nn.GELU()
-#         self.fc2 = nn.Linear(hidden_features, out_features)
-#
-#     def forward(self, x):
-#         x = self.fc1(x)
-#         x = self.act(x)
-#         x = self.fc2(x)
-#         return x
-#
-#
-# class Block(nn.Module):
-#     def _init_(self, embed_dim, num_heads, mlp_ratio=4., qkv_bias=False, drop_rate=0.):
-#         super()._init_()
-#         self.norm1 = nn.LayerNorm(embed_dim, eps=1e-6)
-#         self.attn = Attention(embed_dim, num_heads)
-#         self.norm2 = nn.LayerNorm(embed_dim, eps=1e-6)
-#         mlp_hidden_dim = int(embed_dim * mlp_ratio)
-#         self.mlp = MLP(in_features=embed_dim, hidden_features=mlp_hidden_dim, out_features=embed_dim)
-#
-#     def forward(self, x):
-#         x = x + self.attn(self.norm1(x))
-#         x = x + self.mlp(self.norm2(x))
-#         return x
-#
-#
-# class XRayViT(nn.Module):
-#     def _init_(self, img_size=224, patch_size=16, in_channels=3, embed_dim=768, depth=12, num_heads=12,
-#                mlp_ratio=4., num_classes=1, dropout_p=0.4):
-#         super()._init_()
-#         self.num_patches = (img_size // patch_size) ** 2
-#
-#         self.patch_embed = PatchEmbedding(in_channels, patch_size, embed_dim)
-#
-#         self.cls_token = nn.Parameter(torch.zeros(1, 1, embed_dim))
-#         self.pos_embed = nn.Parameter(torch.zeros(1, self.num_patches + 1, embed_dim))
-#
-#         self.pos_drop = nn.Dropout(p=dropout_p)
-#
-#         self.blocks = nn.ModuleList([
-#             Block(embed_dim, num_heads, mlp_ratio)
-#             for _ in range(depth)
-#         ])
-#
-#         self.norm = nn.LayerNorm(embed_dim)
-#         self.head = nn.Linear(embed_dim, num_classes)
-#
-#     def forward_features(self, x):
-#         x = self.patch_embed(x)
-#
-#         cls_token = self.cls_token.expand(x.shape[0], -1, -1)
-#         x = torch.cat((cls_token, x), dim=1)
-#
-#         x = x + self.pos_embed
-#         x = self.pos_drop(x)
-#
-#         for blk in self.blocks:
-#             x = blk(x)
-#
-#         x = self.norm(x)
-#         return x[:, 0]
-#
-#     def forward(self, x):
-#         x = self.forward_features(x)
-#         x = self.head(x)
-#         return x.squeeze(1)
-
-# -----------------------------
-# CNN model (BatchNorm or LayerNorm injected)
-# -----------------------------
 class XRayCNN(nn.Module):
     def __init__(self, num_classes, norm_layer):
         super().__init__()
@@ -181,13 +69,8 @@ class XRayCNN(nn.Module):
             self._block_mp(128, 128, norm_layer, kernel_size_mp=2),
 
             self._block(128, 256, norm_layer),
-            # Removed last _block_mp to prevent feature map from becoming too small
-            # self._block_mp(256, 256, norm_layer, kernel_size_mp=2),
-
-            # nn.AdaptiveAvgPool2d((1,1))
         )
 
-        # NEW: force to 1×1 spatially
         self.gap = nn.AdaptiveAvgPool2d((1, 1))
 
         self.classifier = nn.Sequential(
